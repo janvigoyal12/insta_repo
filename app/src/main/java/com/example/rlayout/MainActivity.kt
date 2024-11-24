69e6859de10d56df84d68c689bafb78e74e2702e
janvigoyal12 <goyaljanvi77196@gmail.com>
2024-11-24 18:45:15 +0530

    Descriptive message about the changes

--- app/src/main/java/com/example/rlayout/MainActivity.kt
+++ app/src/main/java/com/example/rlayout/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.rlayout
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+         val login:Button=findViewById(R.id.login)
+          login.setOnClickListener {
+              Toast.makeText(this,"buttonClicked!",Toast.LENGTH_SHORT).show()
+
+
+          }
     }
 }
